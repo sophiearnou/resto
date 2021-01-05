700df248fedb55b57b30b6fe429e1caff93d6db0
Sophie Arnou <sophiearnou@yahoo.fr>
2021-01-05 15:59:12 +0100

    animation

--- script.js
+++ script.js
@@ -18,3 +18,35 @@ function toggleMenu() {
     menuToggle.classList.toggle('active');
     navigation.classList.toggle('active');
 }
+
+/*========== ON SCROLL ANIMATION ==========*/
+
+let scroll = window.requestAnimationFrame || function(callback) {
+    window.setTimeout(callback, 1000 / 60)
+}
+
+let elToShow = document.querySelectorAll('.play-on-scroll')
+
+isElInViewPort = (el) => {
+    let rect = el.getBoundingClientRect()
+
+    return (
+        (rect.top <= 0 && rect.bottom >= 0) ||
+        (rect.bottom >= (window.innerHeight || document.documentElement.clientHeight) && rect.top <= (window.innerHeight || document.documentElement.clientHeight)) ||
+        (rect.top >= 0 && rect.bottom <= (window.innerHeight || document.documentElement.clientHeight))
+    )
+}
+
+loop = () => {
+    elToShow.forEach((item, index) => {
+        if (isElInViewPort(item)) {
+            item.classList.add('start')
+        } else {
+            item.classList.remove('start')
+        }
+    })
+
+    scroll(loop)
+}
+
+loop()
